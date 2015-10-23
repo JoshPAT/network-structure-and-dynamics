3c6e7e86c2aeb92445595f23d80df4bcf8d3aa8b
QuanZhou <joshzhou16@gmail.com>
2015-10-23 02:11:07 +0200

    save before change

--- exercise_4/linkprediction.py
+++ exercise_4/linkprediction.py
@@ -3,13 +3,19 @@
 
 __author__ = 'Quan zhou'
 
-import os
+import os, shutil, argparse, logging, functools, time, collections, math, itertools, random, operator
 import numpy as np
-import argparse
-import logging
-import functools
-from graph import run_time
-from collections import *
+
+# a dectorator used to compute run time in a fucntion
+def run_time(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        start_time = time.time()
+        result = func(*args, **kw)
+        end_time = time.time()
+        print "Computation Time of %s: %s" % (func.__name__.capitalize(), end_time - start_time)
+        return result
+    return wrapper
 
 # A Fast Way to create a dir
 def mkdir(func):
@@ -18,7 +24,7 @@ def mkdir(func):
         # Create a dir
         global dirpath
         dir_name = func.__name__
-        dirpath = os.path.join('datasets/', dir_name)
+        dirpath = os.path.join('outputs/', dir_name)
         if not os.path.exists(dirpath):
             os.makedirs(dirpath)
         result = func(*args, **kw)
@@ -57,9 +63,10 @@ def parse_args():
         "--method",
         type=str,
         nargs="?",
-        help="Execute the Link Prediction method. \n \
-              'j' stands for jaccard ranking. \
-              ",
+        help="Execute the Link Prediction method. \
+              'lj' stands for jaccard ranking.   \
+              'la' stands for adamic adar ranking.  \
+              '' ",
         default=None
     )
     return parser.parse_args()
@@ -73,6 +80,7 @@ def data_preparation(args):
     '''
     global nodes_number
     global num_missed_links
+
     f = args.dataset
     num_missed_links = args.number
     reset = args.reset
@@ -82,18 +90,17 @@ def data_preparation(args):
         [line.strip().split(' ') for line in f.readlines()],
         dtype = np.int16
     )
-    
+
     # To have the global number of the nodes
     nodes_number = np.amax(orginal_links)
 
-    if not os.path.exists('datasets/missed_links.txt') or \
-        not os.path.exists('datasets/sample_links.txt') or \
-        reset:
-        if reset:
-            logging.info('Reset files. Start to Create...')
-        else:
-            logging.info('No files. Start to Create...')
-        
+    # Reset - Delete all the files and dirs except datafile
+    if reset:
+        if os.path.exists('outputs'): 
+            logging.info('Deleting old files...')
+            shutil.rmtree('outputs')
+        os.makedirs('outputs')
+
         # This is much faster way to get the data, not check one by one
         randlist = np.random.randint(len(orginal_links), size=num_missed_links + num_missed_links/4)
         randlist = np.unique(randlist)[:num_missed_links]
@@ -103,24 +110,48 @@ def data_preparation(args):
         # To Save both the sample links and missed links
         filepath = os.path.split(os.path.abspath(__file__))[0]
 
-        with open(os.path.join(filepath, 'datasets/missed_links.txt'),
+        with open(os.path.join(filepath, 'outputs/missed_links.txt'),
                      'w') as file_1:
             for row in missed_links:
                 file_1.write('%d %d\n' % (row[0], row[1]))
             logging.info('Successfully generate the missed links file')
 
-        with open(os.path.join(filepath, 'datasets/sample_links.txt'),
+        with open(os.path.join(filepath, 'outputs/sample_links.txt'),
                      'w') as file_2:
             for row in sample_links:
                 file_2.write('%d %d\n' % (row[0], row[1]))
             logging.info('Successfully generate the sample links file')
 
+def nodes_edges(option=False):
+    with open('outputs/sample_links.txt', 'r') as f:
+        sample_links = np.array(
+            [line.strip().split(' ') for line in f.readlines()],
+            dtype = np.int16
+        )
+    # Create set or lists of each nodes
+    if option:
+        d = collections.defaultdict(list)
+        for i,j in sample_links:
+            if i!= j:
+                d[i].append(j)
+                d[j].append(i)
+    else:
+        d = collections.defaultdict(set)
+        for i,j in sample_links:
+            if i!= j:
+                d[i].add(j)
+                d[j].add(i)
+    return d
+
 
 class Local_Scoring():
     @staticmethod
     @mkdir
+    @run_time
     def jaccard_ranking():
         '''
+        This method is used to calculate the jaccard_ranking.
+
         In numpy, it enables to sort array by certain name:
         
         j_links = np.array(
@@ -141,22 +172,9 @@ class Local_Scoring():
         result_file = os.path.join(dirpath, 'results.txt')
 
         if not os.path.exists(result_file):
-
             logging.info('Start to Compute...')
-            # Read the sample file
-            with open('datasets/sample_links.txt', 'r') as f:
-                sample_links = np.array(
-                    [line.strip().split(' ') for line in f.readlines()],
-                    dtype = np.int16
-                )
-                
-            # Create set of each nodes
-            d = defaultdict(set)
-            for i,j in sample_links:
-                if i!= j:
-                    d[i].add(j)
-                    d[j].add(i)
-
+            # Load the edges 
+            d = nodes_edges()
             j_links = []
             for i in xrange(nodes_number+1):
                 for j in xrange(i, nodes_number+1):
@@ -178,17 +196,123 @@ class Local_Scoring():
                     f.write('%d %d %f\n' % (i, j, s))
 
     @staticmethod
+    @mkdir
+    @run_time
     def adamic_adar_ranking():
-        pass
+        '''
+        This method is used to calculate the adamic_adar_ranking.
+
+        '''
+        
+        result_file = os.path.join(dirpath, 'results.txt')
+
+        if not os.path.exists(result_file):
+            logging.info('Start to Compute...')
+            # Load the edges 
+            d = nodes_edges()
+            j_links = []
+            for i in xrange(nodes_number+1):
+                for j in xrange(i, nodes_number+1):
+                    if i != j and j not in d[i]:
+                        # Union = d[i] | d[j]
+                        intersection = d[i] & d[j]
+                        if intersection:
+                            scores = 0
+                            for internode in intersection:
+                                scores += 1.0 / math.log(len(d[internode]))
+                            j_links.append([i,j,scores]) 
+
+            # Get the links ranked nth
+            j_links = np.array(j_links)
+            ranks = np.argsort(j_links[:,2], kind = 'heapsort')[::-1]
+            j_links = j_links[ranks]
+            logging.info('Finished.') 
+
+            with open(result_file, 'w') as f:
+                for i, j, s in j_links:
+                    f.write('%d %d %f\n' % (i, j, s))
 
     @staticmethod
+    @mkdir
+    @run_time
     def resource_allocation_ranking():
-        pass
+        '''
+        This method is used to calculate the resource_allocation_ranking.
+
+        '''
+        result_file = os.path.join(dirpath, 'results.txt')
+
+        if not os.path.exists(result_file):
+            logging.info('Start to Compute...')
+            # Load the edges 
+            d = nodes_edges()
+            j_links = []
+            for i in xrange(nodes_number+1):
+                for j in xrange(i, nodes_number+1):
+                    if i != j and j not in d[i]:
+                        # Union = d[i] | d[j]
+                        intersection = d[i] & d[j]
+                        if intersection:
+                            scores = 0
+                            for internode in intersection:
+                                scores += 1.0 / len(d[internode])
+                            j_links.append([i,j,scores]) 
+
+            # Get the links ranked nth
+            j_links = np.array(j_links)
+            ranks = np.argsort(j_links[:,2], kind = 'heapsort')
+            j_links = j_links[ranks][-num_missed_links:]
+            logging.info('Finished.') 
+
+            with open(result_file, 'w') as f:
+                for i, j, s in j_links:
+                    f.write('%d %d %f\n' % (i, j, s))        
 
 class Global_Scoring():
     @staticmethod
+    @mkdir
+    @run_time
     def random_paths_scoring():
-        pass
+        '''
+        In order to generate 
+        '''
+        
+        result_file = os.path.join(dirpath, 'results.txt')
+        if not os.path.exists(result_file):
+            logging.info('Start to generate random path...')
+            d = nodes_edges(option=True)
+            pair_sets = collections.defaultdict(int)
+            for _ in xrange(10 ** 6):
+                while 1:
+                    # Random generate a node
+                    node = random.randint(0,nodes_number + 1)
+                    random_path = [node]
+                    # Loop until there is a dead end or fullfill the condtion
+                    while len(d[node]) > 1 and len(random_path) < 4:
+                        next_node = random.choice(d[node])
+                        # Eliminate possible path like 1-2-1-3 or 1-2-1-2
+                        if next_node not in random_path:
+                            random_path += [next_node]
+                            node = next_node
+                        else:
+                            node = random.randint(0,nodes_number + 1)
+                            random_path = [node]
+                    # Break if fullfill the condition
+                    if len(random_path) > 3:
+                        break
+                # Filter the existing link in the path
+                if random_path[0] not in d[random_path[2]]:
+                    pair = tuple(sorted(random_path[::2]))
+                    pair_sets[pair] += 1
+                if random_path[1] not in d[random_path[3]]:
+                    pair = tuple(sorted(random_path[1::2]))
+                    pair_sets[pair] += 1
+
+            pair_sets = sorted(pair_sets.items(), key=operator.itemgetter(1), reverse = True)
+            with open(result_file, 'w') as f:
+                for links, scores in pair_sets:
+                    f.write("%d %d " % links + "%d\n" % scores)
+            logging.info('Finished.')
 
     @staticmethod
     def karz_method():
@@ -202,8 +326,6 @@ class Consensus_Method():
 def precision_recall_plots():
     pass
 
-
-
 if __name__ == '__main__':
     # Set the log level
     logging.basicConfig(level=logging.INFO)
@@ -212,7 +334,11 @@ if __name__ == '__main__':
     # Generate the data
     data_preparation(args)
     if args.method:
-        if args.method =='j':Local_Scoring.jaccard_ranking()
-
+        if args.method == 'lj' : Local_Scoring.jaccard_ranking()
+        if args.method == 'la' : Local_Scoring.adamic_adar_ranking()
+        if args.method == 'lr' : Local_Scoring.resource_allocation_ranking()
+        if args.method == 'gr' : Global_Scoring.random_paths_scoring()
+        if args.method == 'gk' : Global_Scoring.karz_method()
+        if args.method == 'cb' : Consensus_Method.Bordas_method()
 
     
